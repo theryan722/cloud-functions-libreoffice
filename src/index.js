923be563e8ade9397b8dc479763782082cbee35f
Valentin Hăloiu <valentin.haloiu@areo.io>
2018-06-08 16:34:56 +0200

    Add convertToPdf method

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const { execSync } = require('child_process')
+const shellescape = require('shell-escape')
 
 const defaultArgs = [
   '--headless',
@@ -14,4 +16,17 @@ const executablePath = path.normalize(
   path.join(__dirname, '..', 'bin', 'instdir', 'program', 'soffice')
 )
 
-module.exports = { defaultArgs, executablePath }
+function convertToPdf(inputFile, outputDir) {
+  const convertCmd = [
+    executablePath,
+    ...defaultArgs,
+    '--convert-to',
+    'pdf',
+    inputFile,
+    '--outdir',
+    outputDir
+  ]
+  execSync(shellescape(convertCmd))
+}
+
+module.exports = { defaultArgs, executablePath, convertToPdf }
